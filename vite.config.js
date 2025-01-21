911ce17216c1ff76f05094d787f50346852ebd3c
PraneethKulukuri26 <praneethkulukuri@gmail.com>
2025-01-21 16:04:07 +0530

    another

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
+  proxy: {
+    '/api': {
+      target: 'http://180.188.227.250:7070',
+      changeOrigin: true,
+      secure: false,
+    },
+  }
 })
